77111ed0e75439d04d3158477cb1e370315ef4a2
Your Name <lobozinho.ow.2210@gmail.com>
2023-07-10 09:08:24 +0700

    Done BombSpawner

--- Assets/_Script/BombSpawner/BombSpawner.cs
+++ Assets/_Script/BombSpawner/BombSpawner.cs
@@ -14,7 +14,11 @@ public class BombSpawner : Spawner
     {
         if (!InputManager.Instance.PressSpace) return;
 
-
+        Vector3 pos = PlayerCtrl.Instance.transform.position;
+        Quaternion rot = transform.rotation;
+        Transform prefab = this.RandomPrefab();
+        Transform obj = this.Spawn(prefab, pos, rot);
+        obj.gameObject.SetActive(true);
     }
 
 }
