35ad20311355636576681a55878e189a12fe1188
Your Name <lobozinho.ow.2210@gmail.com>
2023-08-15 14:10:55 +0700

    Update CopSpawner

--- Assets/_Script/Spawner/CopSpawner/CopSpawner.cs
+++ Assets/_Script/Spawner/CopSpawner/CopSpawner.cs
@@ -17,7 +17,7 @@ public class CopSpawner : Spawner
     {
         if (this.TimeDelay()) return;
         
-        Transform prefab = this.prefabs[2];
+        Transform prefab = this.RandomPrefab();
         Vector3 copPosition = this.GetCopPosition();
         Transform obj = this.Spawn(prefab, copPosition, Quaternion.identity);
         obj.gameObject.SetActive(true);
