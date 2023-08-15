5bc8e8828c86d0d3d58ff87e9f35a90804609a28
Your Name <lobozinho.ow.2210@gmail.com>
2023-08-15 15:50:23 +0700

    Done ObstacleDespawn

---
+++ Assets/_Script/Spawner/ObstacleSpawner/ObstacleDespawnByDistance.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleDespawnByDistance : ObstacleDespawner
+{
+
+    [SerializeField] private float _distance = 10f;
+
+    private void FixedUpdate()
+    {
+        this.DespawningByDistance();
+    }
+
+    void DespawningByDistance()
+    {
+        if (this.CheckDistance()) return;
+        this.Despawning();
+    }
+
+    bool CheckDistance()
+    {
+        float playerPosY = this.GetPlayerPositionY();
+        float prefabPosY = transform.parent.position.y;
+        if (playerPosY - prefabPosY < this._distance) return true;
+        return false;
+    }
+
+    float GetPlayerPositionY()
+    {
+        return PlayerCtrl.Instance.transform.position.y;
+    }
+
+}
--- Assets/_Script/Spawner/SpawnerCtrl.cs
+++ Assets/_Script/Spawner/SpawnerCtrl.cs
@@ -8,7 +8,13 @@ public class SpawnerCtrl : LoboMonoBehaviour
     public static SpawnerCtrl Instance => _instance; 
 
     [SerializeField] private RoadSpawner _roadSpawner;
-    public RoadSpawner RoadSpawner => _roadSpawner; 
+    public RoadSpawner RoadSpawner => _roadSpawner;
+
+    [SerializeField] private CopSpawner _copSpawner;
+    public CopSpawner CopSpawner => _copSpawner;
+
+    [SerializeField] private ObstacleSpawner _obstacleSpawner;
+    public ObstacleSpawner ObstacleSpawner => _obstacleSpawner;
 
     protected override void Awake()
     {
@@ -20,6 +26,8 @@ public class SpawnerCtrl : LoboMonoBehaviour
     {
         base.LoadComponents();
         this.LoadRoadSpawner();
+        this.LoadCopSpawner();
+        this.LoadObstacleSpawner();
     }
 
     protected virtual void LoadRoadSpawner()
@@ -27,6 +35,20 @@ public class SpawnerCtrl : LoboMonoBehaviour
         if (this._roadSpawner != null) return;
         this._roadSpawner = GetComponentInChildren<RoadSpawner>();
         Debug.Log(transform.name + ": LoadRoadSpawner", gameObject);
+    }
+
+    protected virtual void LoadCopSpawner()
+    {
+        if (this._copSpawner != null) return;
+        this._copSpawner = GetComponentInChildren<CopSpawner>();
+        Debug.Log(transform.name + ": LoadCopSpawner", gameObject);
+    }
+
+    protected virtual void LoadObstacleSpawner()
+    {
+        if (this._obstacleSpawner != null) return;
+        this._obstacleSpawner = GetComponentInChildren<ObstacleSpawner>();
+        Debug.Log(transform.name + ": LoadObstacleSpawner", gameObject);
     }
 
 }
