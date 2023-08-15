40a161bc1fda03ad7e36736b9f001020a922a242
Your Name <lobozinho.ow.2210@gmail.com>
2023-08-15 15:03:35 +0700

    Done ObstacleSpawner

--- Assets/_Script/Spawner/CopSpawner/CopSpawner.cs
+++ Assets/_Script/Spawner/CopSpawner/CopSpawner.cs
@@ -2,47 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CopSpawner : Spawner
+public class CopSpawner : SpawnerByTime
 {
-    [Header("Cop Spawner")]
-    [SerializeField] private float _timer;
-    [SerializeField] private float _timeDelay = 5f;
-
-    private void FixedUpdate()
-    {
-        this.CopSpawning();
-    }
-
-    void CopSpawning()
-    {
-        if (this.TimeDelay()) return;
-        
-        Transform prefab = this.RandomPrefab();
-        Vector3 copPosition = this.GetCopPosition();
-        Transform obj = this.Spawn(prefab, copPosition, Quaternion.identity);
-        obj.gameObject.SetActive(true);
-    }
-
-    bool TimeDelay()
-    {
-        this._timer += Time.fixedDeltaTime;
-        if(this._timer < this._timeDelay) return true;
-        this._timer = 0;
-        return false;
-    }
-
-    Vector3 GetCopPosition()
-    {
-        Vector3 playerPosition = this.GetPlayerPosition();
-
-        Vector3 copPosition = playerPosition;
-        copPosition.y = playerPosition.y - MainCamera.Instance.CameraSize * 2;
-        return copPosition;
-    }
-
-    Vector3 GetPlayerPosition()
-    {
-        return PlayerCtrl.Instance.transform.position;
-    }
     
+    protected override void ResetValue()
+    {
+        base.ResetValue();
+        this.timeDelay = 5f;
+        this.offset = -20f;
+    }
+
 }
---
+++ Assets/_Script/Spawner/SpawnerByTime.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerByTime : Spawner
+{
+    [SerializeField] protected float timer;
+    [SerializeField] protected float timeDelay;
+    [SerializeField] protected float offset;
+
+    protected virtual void FixedUpdate()
+    {
+        this.Spawning();
+    }
+
+    protected virtual void Spawning()
+    {
+        if (this.TimeDelay()) return;
+
+        Transform prefab = this.RandomPrefab();
+        Vector3 spawnPosition = this.GetSpawnPosition();
+        Transform obj = this.Spawn(prefab, spawnPosition, Quaternion.identity);
+        obj.gameObject.SetActive(true);
+    }
+    protected virtual bool TimeDelay()
+    {
+        this.timer += Time.fixedDeltaTime;
+        if (this.timer < this.timeDelay) return true;
+        this.timer = 0;
+        return false;
+    }
+
+    protected virtual Vector3 GetSpawnPosition()
+    {
+        Vector3 spawnPosition = this.GetPlayerPosition();
+        spawnPosition.y += this.offset;
+        spawnPosition.x = this.RamdomSpawnPosition();
+        return spawnPosition;
+    }
+
+    protected virtual float RamdomSpawnPosition()
+    {
+        return 0;
+    }
+
+    protected virtual Vector3 GetPlayerPosition()
+    {
+        return PlayerCtrl.Instance.transform.position;
+    }
+
+}
